9340317fc220813e56246c0f08c35c28702965f5
xilujain <lujainboo@gmail.com>
2023-05-01 13:21:49 +0300

    add task1 file

---
+++ task1.py
@@ -0,0 +1,30 @@
+# number of rows
+rows = 5
+
+# print stars
+for i in range(rows+1):
+    for j in range(i):
+        print("*", end=" ")
+    print(" ")
+
+# print new line
+print(" ")
+
+# print stars reverse
+for i in range(rows, 0, -1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print(" ")
+
+# print new line
+print(" ")
+
+#print stars in one loop
+i = 1
+while i <= rows:
+    j = 1
+    while j <= i:
+        print("*", end=" ")
+        j = j + 1
+    i = i + 1
+    print(" ")
